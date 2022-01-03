ef638e753b1a8e1b7eb1b9c3be7ec08d8f2b4824
liu-lab-asus <liu246542@163.com>
2022-01-03 18:35:19 +0800

    :memo: INIT

---
+++ play.py
@@ -0,0 +1,69 @@
+import subprocess
+import time
+import os
+import json
+from utils.blivex import Bilibili
+
+bili = Bilibili()
+bili.login_with_cookie('./data/cookie.json')
+
+while not bili.info["live_status"]:
+    bili.start_live()
+    time.sleep(5)
+    bili.get_user_info()
+
+rtmp_addr = bili.get_rtmp()
+
+with open('./data/videos.json') as v_f:
+    live_list = json.load(v_f)
+
+file_list = []
+cursor = int(live_list['cursor'])
+
+for p in live_list['path']:
+    f_li = [x for x in os.listdir(p) if (os.path.splitext(x)[1] == '.mp4')]
+    f_li = sorted(f_li, key=lambda a: int(a[:-4]))
+    file_list.extend( p+x for x in f_li)
+
+pushList = file_list[cursor:] + file_list[:cursor] # 整合播放列表
+
+startpoint = '00:00:00'
+
+i = 0
+retry = 5 # 断开重连次数
+
+while True:
+    log_file = open('./data/live.log', 'a')
+    print("*"*5 + "-"*5 + "*"*5)
+    e_start = time.time()
+    print("正在播放" + pushList[i])
+    log_content = (f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}]  {pushList[i]}")
+    log_file.write(log_content + "\n")
+    log_file.close()
+
+    p = subprocess.Popen('ffmpeg -re -ss ' + startpoint + ' -i ' + pushList[i] + ' -c copy -shortest -stimeout 120000 -f flv \"' + rtmp_addr + '\" ', shell=True)
+    p.wait()
+
+    e_end = time.time()
+    playtime = int(startpoint[3:5]) * 60 + int(startpoint[6:8]) + (e_end - e_start) # 计算单集视频的播放时长
+
+    if retry == 0: # 超过重连次数，关闭直播间
+        bili.stop_live()
+        break
+
+    if playtime < 2100: # 如果单集播放时长不足 35 分钟 2100 s = 35 min，则认为直播被断开，开始重启直播间
+        bili.stop_live()
+        time.sleep(2)
+        bili.start_live()
+        retry = retry - 1
+        startpoint = time.strftime('%H:%M:%S', time.gmtime(playtime)) # 计算上次播放位置
+        continue
+
+    i += 1
+    startpoint = '00:00:00'
+    retry = 5
+    live_list['cursor'] = (cursor + i) % len(pushList)
+    with open('./data/videos.json', 'w') as w_f:
+        json.dump(live_list, w_f)
+    if i == len(pushList):
+        i = 0
---
+++ utils/blivex.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+
+class Bilibili:
+    def __init__(self):
+        self._session = requests.Session()
+        self.get_cookies = lambda: self._session.cookies.get_dict(domain=".bilibili.com")
+        self.get_uid = lambda: self.get_cookies().get("DedeUserID", "")
+        self.info = {
+            'ban': False,
+            'coins': 0,
+            'face': "",
+            'level': 0,
+            'nickname': "",
+            'room_id': "",
+            'live_status': False
+        }
+        self.headers = {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "referrer": "https://link.bilibili.com/p/center/index",
+            "referrerPolicy": "no-referrer-when-downgrade"
+        }
+
+    @staticmethod
+    def _log(message):
+        print(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}] {message}")
+
+    def _requests(self, method, url, decode_level=2, retry=0, timeout=10, **kwargs):
+        if method in ["get", "post"]:
+            for _ in range(retry + 1):
+                try:
+                    response = getattr(self._session, method)(url, timeout=timeout, **kwargs)
+                    return response.json() if decode_level == 2 else response.content if decode_level == 1 else response
+                except:
+                    pass
+        return None
+
+    # 使用 cookie 登陆
+    def login_with_cookie(self, fcookie):
+        import json
+        with open(fcookie) as f:
+            tempCookie = json.load(f)
+        for k in tempCookie.keys():
+            self._session.cookies.set(k, tempCookie[k], domain=".bilibili.com")
+        if self.get_user_info():
+            self._log("登录成功")
+        return True
+    
+    # 获取用户信息
+    def get_user_info(self):
+        url = f"https://api.bilibili.com/x/space/acc/info?mid={self.get_uid()}&jsonp=jsonp"
+        headers = {
+            'Host': "api.bilibili.com",
+            'Referer': f"https://space.bilibili.com/{self.get_uid()}/",
+        }
+        response = self._requests("get", url, headers=headers)
+        if response and response.get("code") == 0:
+            self.info['ban'] = bool(response['data']['silence'])
+            self.info['coins'] = response['data']['coins']
+            self.info['face'] = response['data']['face']
+            self.info['level'] = response['data']['level']
+            self.info['nickname'] = response['data']['name']
+            self.info['room_id'] = response['data']['live_room']['roomid']
+            self.info['live_status'] = bool(response['data']['live_room']['liveStatus'])
+            # self.room_info = self._requests("get", "https://api.live.bilibili.com/xlive/app-blink/v1/room/GetInfo?platform=pc").get("data")
+            self._log(f"{self.info['nickname']}(UID={self.get_uid()}), Lv.{self.info['level']}, 拥有{self.info['coins']}枚硬币, 账号{'状态正常' if not self.info['ban'] else '被封禁'}, 直播间ID={self.info['room_id']}, {'正在直播' if self.info['live_status'] else '停播状态'}")
+            return True
+        else:
+            self._log("用户信息获取失败")
+            return False
+
+    def start_live(self):                
+        url = "https://api.live.bilibili.com/room/v1/Room/startLive"
+        payload = {
+            'room_id': self.info['room_id'],
+            'platform': 'pc',
+            'area_v2': 33, # 此处可以手动设置，如，33: 影音馆，376: 学习-人文社科
+            'csrf_token': self._session.cookies['bili_jct'],
+            'csrf': self._session.cookies['bili_jct'],
+        }
+        response = self._requests("post", url, data=payload, headers=self.headers).get("data")
+        self.rtmp_addr = response.get("rtmp").get("addr") + response.get("rtmp").get("code")
+        if not self.rtmp_addr:
+            self._log("开启直播间失败")
+            return False
+        self._log("开启直播间成功，串流地址为：" + self.rtmp_addr)
+        return True
+
+    def get_rtmp(self):
+        url = "https://api.live.bilibili.com/xlive/app-blink/v1/live/getWebUpStreamAddr?platform=pc"
+        response = self._requests("get", url).get("data").get("addr")
+        self.rtmp_addr = response.get("addr") + response.get("code")
+        return self.rtmp_addr
+
+    def stop_live(self):
+        url = "https://api.live.bilibili.com/room/v1/Room/stopLive"
+        payload = {
+            'room_id': self.info['room_id'],
+            'platform': 'pc',
+            'csrf_token': self._session.cookies['bili_jct'],
+            'csrf': self._session.cookies['bili_jct'],
+        }
+        response = self._requests("post", url, data=payload, headers=self.headers)
+        self._log("正在关闭直播间")
+        return True
